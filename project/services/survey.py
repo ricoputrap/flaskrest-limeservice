29bb41251d3c8709e15815dbff6520c531217112
Favian Kharisma Hazman <vianhazman@gmail.com>
2021-11-15 00:51:29 +0700

    feat: add parallel delete and participant fetch limit

--- project/services/survey.py
+++ project/services/survey.py
@@ -1,8 +1,16 @@
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
+
+from citric.exceptions import LimeSurveyStatusError
+
 from project.models.survey import SurveyModel
 from project.clients.limesurvey.api import LimesurveyClient
 
-
 # from utils import db
+
+# Good number of concurrency to start with
+SURVEY_PARTICIPANT_DELETE_CONCURRENCY = 40
+
 
 class SurveyService:
     client = LimesurveyClient()
@@ -14,8 +22,22 @@ class SurveyService:
         # surveys = []
         return available_surveys
 
+    def get_list_survey_participants(self, survey_id):
+        return self.client.list_participants(survey_id)
+
+    def _delete_survey_participant(self, survey_id, participant):
+        participant_id = participant.get("tid")
+        res = self.client.delete_survey_participant(survey_id, participant_id)
+        print(res)
+        return res
+
     def truncate_survey_participants(self, survey_id, participant_data):
-        current_participants = self.client.list_participants(survey_id)
+        try:
+            current_participants = self.client.list_participants(survey_id)
+        except LimeSurveyStatusError:
+            current_participants = []
         if len(current_participants) > 0:
-            self.client.delete_survey_participant(survey_id, current_participants)
+            # parallel execution since we need to hit api one by one for delete
+            pool = ThreadPoolExecutor(SURVEY_PARTICIPANT_DELETE_CONCURRENCY)
+            pool.map(self._delete_survey_participant, repeat(survey_id), current_participants)
         self.client.add_survey_participant(survey_id, participant_data)
