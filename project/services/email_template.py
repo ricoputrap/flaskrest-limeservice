35e5b4a3f1de171732454f06998bfd88641c553b
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-12-06 06:13:02 +0700

    feat(survey): implement helper function to validate email template

--- project/services/email_template.py
+++ project/services/email_template.py
@@ -1,40 +1,19 @@
 from project.models.email_template import EmailTemplateModel
+from project.services.helpers import validate_email_template
 from project.utils import db
 
 class EmailTemplateService:
 
     def create_email_template(self, req_body):
-        name = req_body["name"] if "name" in req_body else ""
-        subject = req_body["subject"] if "subject" in req_body else ""
-        body = req_body["body"] if "body" in req_body else ""
 
-        # populate empty fields
-        empty_fields = []
-        if not name:
-            empty_fields.append({ "field": "name", "value": name })
-        if not subject:
-            empty_fields.append({ "field": "subject", "value": subject })
-        if not body:
-            empty_fields.append({ "field": "body", "value": body })
-        
-        # return error if any field empty
-        if len(empty_fields) > 0:
-            res = { 
-                "status_code": 400,
-                "error": {
-                    "title": "Bad Request"
-                }
-            }
-            if len(empty_fields) == 1:
-                error_detail = "Missing the value of " + empty_fields[0]["field"]
-            elif len(empty_fields) == 2:
-                error_detail = "Missing the values of " + empty_fields[0]["field"] \
-                    + " and " + empty_fields[1]["field"]
-            else:
-                error_detail = "Missing the values of " + empty_fields[0]["field"] \
-                    + ", " + empty_fields[1]["field"] + ", and" + empty_fields[2]["field"]
-            res["error"]["detail"] = error_detail
+        res = validate_email_template(req_body)
+        if "error" in res:
             return res
+        
+        # extract the values
+        name = res["name"]
+        subject = res["subject"]
+        body = res["body"]
         
         # if name already exists
         email_template = EmailTemplateModel.query.filter_by(name=name).first()
@@ -63,3 +42,20 @@ class EmailTemplateService:
             "subject": subject,
             "body": body,
         }
+
+    def update_email_template(self, req_body):
+        id = req_body["id"] if "id" in req_body else ""
+        email_template = EmailTemplateModel.query.filter_by(id=id).first()
+        if not email_template:
+            return {
+                "status_code": 404,
+                "error": {
+                    "title": "Email Template Not Found",
+                    "detail": "Email template with id " + str(id) + " is not found"
+                }
+            }
+
+        name = req_body["name"] if "name" in req_body else ""
+        subject = req_body["subject"] if "subject" in req_body else ""
+        body = req_body["body"] if "body" in req_body else ""
+
